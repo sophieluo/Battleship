c11232f2456f511951036afbd71d6e581ad63786
sophieluo <sophie@groobusiness.com>
2019-11-17 23:17:34 -0500

    add basic game flow. might need to add try.. later

--- src/battleship/BattleshipGame.java
+++ src/battleship/BattleshipGame.java
@@ -1,9 +1,48 @@
 package battleship;
 
+import java.util.Scanner;
+
+
 public class BattleshipGame {
-
+	
+	/**
+	 * The 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-
+		BattleshipGame game  = new BattleshipGame();
+		while(game.playGame()) {
+			continue;
+		}
 	}
+	
+	public boolean playGame() {
+		Ocean ocean = new Ocean();
+		ocean.placeAllShipsRandomly();
+		Scanner sc = new Scanner(System.in);
+		ocean.print();
+		
+		while(ocean.isGameOver() == false) {
+			System.out.println("Please enter two integers beween 0 and 9, inclusive");
+			int row = sc.nextInt();
+			int column = sc.nextInt();
+			
+			if(ocean.shootAt(row, column)) {
+				System.out.println("hit");
+				Ship[][] ships = ocean.getShipArray();
+				if (ships[row][column].isSunk()) {
+					System.out.println(ships[row][column].getShipType() + " is sunk.");
+				}
+			} else {
+				System.out.println("Missed this shot");
+			}
+			ocean.print();
+		}
+		System.out.println("Final score is " + ocean.getShotsFired());
+		System.out.println("Play again?");
+		String playAgain = sc.nextLine();
+		return playAgain.toLowerCase().startsWith("y");
+	}
+	
 
 }
